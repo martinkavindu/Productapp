343d950e210640077408dfa6f7b189762b6d7988
mutuku1998 <martinmutuku1998@gmail.com>
2024-01-12 04:37:17 +0300

    m

--- products/src/App.js
+++ products/src/App.js
@@ -4,11 +4,18 @@ import Addproduct from './components/Addproduct';
 import ProductList from './components/ProductList';
 import Login from './components/Login';
 import Register from './components/Register';
-;
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
 function App() {
   return (
     <div className='app'>
- <Register/>
+<BrowserRouter>
+<Routes>
+  <Route path='/' element ={<Login/>}/>
+  <Route path='/register' element ={<Register/>}/>
+  <Route path='/allproducts' element = {<ProductList/>}/>
+  <Route path='/addproduct' element = {<Addproduct/>}/>
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
--- products/src/components/Register.js
+++ products/src/components/Register.js
@@ -1,29 +1,61 @@
 import React from 'react'
 import { useState } from 'react'
 import validation from './Registervalidation'
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
 const Register = () => {
-    const[values,setValues ] =useState({
+    const[values,setValues] = useState({
         name:'',
         email:'',
         password:''
     
-    }
-    )
+    })
+
+    console.log(values)
     const[errors,setErrors] = useState({})
     const handleInput = (e)=>{
-        setValues(prev=>({...prev,[e.target.name]:[e.target.value]}))
+        const newdata = { ...values}
+        newdata[e.target.name] = e.target.value
+        setValues(newdata)
      
      }
+     const navigate = useNavigate()
 
      const handleSubmit = (e) =>{
         e.preventDefault()
         setErrors(validation(values));
         
+        // axios.post('http://localhost:3003/register/user', values)
+        // .then(res => {
+        //     // Handle the response from the server
+        // })
+        // .catch(err => console.log(err));
+          
+          let config = {
+            method: 'post',
+            url: 'http://localhost:3003/register/user',
+            headers: { 
+              'Content-Type': 'application/json'
+            },
+            data : values
+          };
+          
+          console.log(config)
+          axios.request(config)
+          .then((response) => {
+          navigate('/')
+            console.log(JSON.stringify(response.data));
+          })
+          .catch((error) => {
+            console.log(error);
+          });
      }
+
+  
 
   return (
     <div className='login'>
-    <h3>Welcome back! Login</h3>
+    <h3>Register account</h3>
 <form action='' onSubmit={handleSubmit}>
 
 <div class="form-group">
@@ -35,17 +67,17 @@ const Register = () => {
 <label for="email">Full Name:</label>
 <input type="text" onChange={handleInput} name='name' value={values.name} class="form-control" id="email"/>
 </div>
-{errors.email  && <span className='text-danger'>{errors.email} </span>}
+{errors.email  && <span className='text-danger'>{errors.name} </span>}
 <div class="form-group">
 <label for="pwd">Password:</label>
 <input type="password" onChange={handleInput} value={values.password} name='password' class="form-control" id="pwd"/>
 </div>
 {errors.password && <span className='text-danger'> {errors.password}</span>}
 <div className='mt-5'>
-<button type="submit" class="btn btn-primary">Login</button>
+<button type="submit" class="btn btn-primary">Register</button>
 </div>
 <div>
-<p>Dont have account <a href=''>Register</a></p>
+<p>Already have account <a href=''>Login</a></p>
 </div>
 </form>
 
