9a3ffdbd5c5fee029bdd1b7d6215f2b425991e1c
mutuku1998 <martinmutuku1998@gmail.com>
2024-01-14 02:05:02 +0300

    m

--- products/src/App.js
+++ products/src/App.js
@@ -6,11 +6,12 @@ import Login from './components/Login';
 import Register from './components/Register';
 import UpdateProduct from './components/UpdateProduct';
 import './App.css'
+import Home from './components/Home';
 
 const isAuthenticated = () => localStorage.getItem('accessToken') !== null;
 
 const PrivateRoute = ({ element, ...rest }) => {
-  return isAuthenticated() ? element : <Navigate to="/" />;
+  return isAuthenticated() ? element : <Navigate to="/login" />;
 };
 
 const App = () => {
@@ -18,7 +19,8 @@ const App = () => {
     <div className="app">
       <Router>
         <Routes>
-          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Home/>} />
           <Route path="/register" element={<Register />} />
           <Route path="/allproducts" element={<PrivateRoute element={<ProductList />} />} />
           <Route path="/addproduct" element={<PrivateRoute element={<AddProduct />} />} />
---
+++ products/src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import HeaderSection from './Headersection'
+import "../styles/home.css"
+const Home = () => {
+  return (
+    <>
+    <HeaderSection/>
+    <section className='main'>
+        <div className='home'>
+        <h3>Snack on fresh fruits and vegetables <br/> for energy boost </h3> 
+        <button className='btn1'>learn more</button>
+        </div>
+
+    </section>
+    </>
+  )
+}
+
+export default Home
